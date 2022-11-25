9dfd4bdf9e7433cdb082a50db2d84f277f5114f7
Jme <JmeJanbarJuniper@gmail.com>
2022-11-25 12:28:16 -0900

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -169,64 +169,69 @@ addLayer("a", {
 		],
 		[
 			"display-text",
-			"(function here)"
+			function() {
+				var items = ""
+				for (var item in this.equipment) {
+					var stats = this.equipment[item]
+					if (stats.atk >= 1)	items.push(item+" | "+stats.name+":\n  attack: "+stats.atk+"\n  attack type: "+stats.type+"\n")
+				}
+				return items
+			}
 		],
 	],
 	
 	// Position
 	row: 0,
-	position: 0,
+	position: 1,
 	
 	// Start data
 	startData() {
 		return {
 			unlocked: true,
-			points: new Decimal(10)
+			points: new Decimal(0),
+			equipment: {
+				weapon: {
+					name: "Wooden sword",
+					atk: 1,
+					type: "melee"
+				}
+			}
 		}
 	},
 	
 	// Resetting basics
-	type: "normal",
-	
-	// Resetting gain calculations
-	baseAmount() {
-		return new Decimal(1)
-	},
-	requires: new Decimal(1),
-	passiveGeneration() {
-		return 1
-	}
+	type: "none",
 })
 
-addLayer("a", {
-	name: "Attack", // This is optional, only used in a few places, If absent it just uses the layer id.
-	symbol: "Atk", // This appears on the layer's node. Default is the id with the first letter capitalized
-	position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-	startData() { return {
-		unlocked: true,
-		points: new Decimal(1),
-	}},
-	color: "#f00",
-	requires() {
-		return 1
-	}, // Can be a function that takes requirement increases into account
-	resource: "attack", // Name of prestige currency
-	baseResource: "", // Name of resource prestige is based on
-	baseAmount() {return new Decimal(0)}, // Get the current amount of baseResource
-	type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-	exponent: 1, // Prestige currency exponent
-	gainMult() { // Calculate the multiplier for main currency from bonuses
-		return new Decimal(1)
-	},
-	gainExp() { // Calculate the exponent on main currency from bonuses
-		return new Decimal(1)
-	},
-	row: 0, // Row the layer is in on the tree (0 is the first row)
-	hotkeys: [],
-	layerShown(){return true},
-	upgrades: {},
-	branches: []
-})
+// addLayer("a", {
+// 	name: "Attack", // This is optional, only used in a few places, If absent it just uses the layer id.
+// 	symbol: "Atk", // This appears on the layer's node. Default is the id with the first letter capitalized
+// 	position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+// 	startData() { return {
+// 		unlocked: true,
+// 		points: new Decimal(1),
+// 	}},
+// 	color: "#f00",
+// 	requires() {
+// 		return 1
+// 	}, // Can be a function that takes requirement increases into account
+// 	resource: "attack", // Name of prestige currency
+// 	baseResource: "", // Name of resource prestige is based on
+// 	baseAmount() {return new Decimal(0)}, // Get the current amount of baseResource
+// 	type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+// 	exponent: 1, // Prestige currency exponent
+// 	gainMult() { // Calculate the multiplier for main currency from bonuses
+// 		return new Decimal(1)
+// 	},
+// 	gainExp() { // Calculate the exponent on main currency from bonuses
+// 		return new Decimal(1)
+// 	},
+// 	row: 0, // Row the layer is in on the tree (0 is the first row)
+// 	hotkeys: [],
+// 	layerShown(){return true},
+// 	upgrades: {},
+// 	branches: []
+// })
 
 // LOCATIONS
 
